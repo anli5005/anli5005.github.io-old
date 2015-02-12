a4b82572d21c96d12840915863a8f18b2a04085d
anli5005 <li.anthony917@gmail.com>
2015-02-11 20:23:28 -0500

    Add more content

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ function animateNavbar() {
 
 function whenReady() {
     animateStartupElements(true, animateNavbar);
+    $("#page-home").hide();
+    $("#page-home").fadeIn(1500);
 }
 
 $(document).ready(whenReady);
