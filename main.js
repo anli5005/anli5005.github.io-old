fd5284301087f576cfe2fdac70fc44aa1e4c122e
anli5005 <li.anthony917@gmail.com>
2015-02-12 21:42:43 -0500

    Came up with crude nav links

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+this.lastTimerId = -1;
+
 function onScroll() {
     var scrollTop = $("body").scrollTop();
     var navTop = $("#navigation-bar").offset().top;
@@ -25,6 +27,46 @@ function whenReady() {
     animateHome();
     $(window).scroll(onScroll);
     onScroll();
+}
+
+function scrollTo(selector) {
+    if (this.lastTimerId >= 0) {
+        window.clearInterval(this.lastTimerId);
+    }
+    
+    var offset = $(selector).offset().top;
+    var body = $("body");
+    var st = body.scrollTop();
+    
+    // Prepare animation blocks
+    function scrollUp() {
+        body.scrollTop(body.scrollTop() - 3);
+    }
+    function scrollDown() {
+        body.scrollTop(body.scrollTop() + 3);
+    }
+    function doNothing() {
+        
+    }
+    
+    // Schedule timeouts
+    var scrollAnimFunction;
+    if (st > offset) {
+        scrollAnimFunction = scrollUp;
+    } else if (st < offset) {
+        scrollAnimFunction = scrollDown;
+    } else {
+        scrollAnimFunction = doNothing;
+    }
+    
+    var timerId = window.setInterval(scrollAnimFunction, 1);
+    function stopInterval() {
+        window.clearInterval(timerId);
+        body.scrollTop(offset);
+    }
+    window.setTimeout(stopInterval, (Math.abs(st - offset) * 3));
+    
+    this.lastTimerId = timerId;
 }
 
 $(document).ready(whenReady);
